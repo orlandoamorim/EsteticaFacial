b63bcb90f771930ac64adfdc97ae8a3228f91ec8
Ricardo Freitas <ricardofreitas@imac-de-ricardo.local>
2015-10-05 16:52:18 -0700

    Initial Commit

---
+++ EsteticaFacial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EsteticaFacial
+//
+//  Created by Ricardo Freitas on 05/10/15.
+//  Copyright © 2015 Ricardo Freitas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
